ce1ae6cc24cbdc567fe057b2d0e10ff1fc385691
19tbusst <19tbusst@wakatipu.school.nz>
2023-06-01 14:31:32 +1200

    Made input system more dynamic
    Added a PlayerInputActions class to make Input more dynamic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private InputAction playerControls;
+    private PlayerInputActions playerControls;
 
     [SerializeField]
     private Rigidbody2D rb;
@@ -15,14 +15,28 @@ public class PlayerController : MonoBehaviour
     private float moveSpeed = 500f;
     private Vector2 moveDirection = Vector2.zero;
 
-    private void OnDisable()
+    private InputAction move;
+    private InputAction fire;
+
+    private void Awake()
     {
-        playerControls.Disable();
+        playerControls = new PlayerInputActions();
     }
 
     private void OnEnable()
     {
-        playerControls.Enable();
+        move = playerControls.Player.Move;
+        move.Enable();
+
+        fire = playerControls.Player.Fire;
+        fire.Enable();
+        fire.performed += Fire;
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+        fire.Disable();
     }
 
     // Start is called before the first frame update
@@ -34,11 +48,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveDirection = playerControls.ReadValue<Vector2>();
+        moveDirection = move.ReadValue<Vector2>();
     }
 
     private void FixedUpdate()
     {
         rb.velocity = moveDirection * moveSpeed * Time.fixedDeltaTime;
+    }
+
+    private void Fire(InputAction.CallbackContext context)
+    {
+        Debug.Log("We fired");
     }
 }

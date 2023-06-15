31995441ea2485e069c4c7340c153d30d95ed0aa
19tbusst <19tbusst@wakatipu.school.nz>
2023-06-15 12:59:45 +1200

    added isMoving flag

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -6,6 +6,7 @@ public class PlayerAnimation : MonoBehaviour
 {
     private Animator animator;
     private Vector3 moveDirection;
+    private bool isMoving;
 
     // Start is called before the first frame update
     void Start()
@@ -18,10 +19,14 @@ public class PlayerAnimation : MonoBehaviour
     {
         moveDirection = GetComponent<PlayerController>().moveDirection;
 
+        isMoving = false;
         if (moveDirection.magnitude > 0)
         {
+            isMoving = true;
             animator.SetFloat("X", moveDirection.x);
             animator.SetFloat("Y", moveDirection.y);
         }
+
+        animator.SetBool("isMoving", isMoving);
     }
 }

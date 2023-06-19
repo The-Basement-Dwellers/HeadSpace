da9fc89198a8a4f94cae333450e3c69758021458
19jmodina <19jmodina@wakatipu.school.nz>
2023-06-20 11:44:10 +1200

    added very basic pathfinding

--- Assets/Scripts/InteractablesManager.cs
+++ Assets/Scripts/InteractablesManager.cs
@@ -12,23 +12,20 @@ public class InteractablesManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 
     public void Doors()
     {
-        if (Vector3.Distance(interactscript.player.transform.position, interactscript.closestObject.transform.position) < 3 && interactscript.closestObject.GetComponent<BoxCollider2D>() == true)
+        if (Vector3.Distance(interactscript.player.transform.position, interactscript.closestObject.transform.position) <= 1.5 && interactscript.closestObject.GetComponent<BoxCollider2D>().isTrigger)
         {
-            Debug.Log("Interacted");
-            Destroy(interactscript.closestObject.GetComponent<BoxCollider2D>());
-
+            Debug.Log("Frick");
+            interactscript.closestObject.GetComponent<BoxCollider2D>().isTrigger = false;
+       
         }
         else
         {
-            Debug.Log("fuck");
-            interactscript.closestObject.AddComponent<BoxCollider2D>();
+            Debug.Log("Interacted");
+            interactscript.closestObject.GetComponent<BoxCollider2D>().isTrigger = true;
         }
     }
 }

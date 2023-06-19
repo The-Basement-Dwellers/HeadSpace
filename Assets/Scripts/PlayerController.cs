4d72c9f7fa0cd6936dea4afe4766890a57b6eba6
19tbusst <19tbusst@wakatipu.school.nz>
2023-06-19 20:11:53 +1200

    Revert "Fixed weird merge"
    This reverts commit f4e7935e13430c289a7c7d4213a73ce187c41985.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,12 +17,18 @@ public class PlayerController : MonoBehaviour
     private Vector2 moveDirection = Vector2.zero;
     private Vector2 lookDirection = Vector2.zero;
 
+    // [SerializeField]
+    // private GameObject pointer;
+
     [SerializeField]
     private GameObject cameraWeapon;
 
     [SerializeField]
     private float moveSpeed = 500f;
 
+    private InputAction move;
+    private InputAction fire;
+    private InputAction dash;
     private InputAction interact;
     private InteractablesManager interManager;
     
@@ -161,6 +167,7 @@ public class PlayerController : MonoBehaviour
     private void Dash(InputAction.CallbackContext context) {
         EventController.Dash();
     }
+}
 
     private void Interact(InputAction.CallbackContext context)
     {

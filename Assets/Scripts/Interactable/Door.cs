80c9d2e8ace88556d91970dceb81d0d744613dd0
19jmodina <19jmodina@wakatipu.school.nz>
2023-07-27 17:32:03 +1200

    Doors update pathfinding

--- Assets/Scripts/Interactable/Door.cs
+++ Assets/Scripts/Interactable/Door.cs
@@ -2,22 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static PlayerInteraction;
+using Pathfinding;
 
 public class Door : MonoBehaviour, IInteractable
 {
-	public void Interact()
+	public GraphUpdateScene gustavofring;
+
+	void Start()
+	{
+		gustavofring = gameObject.GetComponent<GraphUpdateScene>();
+	}
+    public void Interact()
 	{
 		if  (gameObject.GetComponent<BoxCollider2D>().isTrigger)
 		{
 			gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
 			gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
-			gameObject.layer = 0;
+			gameObject.layer = 4;
+			
+
 		}
 		else
 		{
 			gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
 			gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .5f);
 			gameObject.layer = 2;
-		}  
-	}
+		}
+        gustavofring.Apply();
+    }
 }
